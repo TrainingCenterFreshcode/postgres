0bfe45a57d3cee0500ea55bc4ff8a00bb54fa145
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-04-19 19:07:52 +0300

    Creating table

--- postres-students.session.sql
+++ postres-students.session.sql
@@ -1,3 +1,22 @@
-CREATE TABLE test();
+CREATE TABLE users_with_emails();
+
+DROP TABLE users, users_with_emails, test;
+
+DROP TABLE products;
 
 -- Як виконати запит: Ctrl + E (2)
+
+/*
+
+char(5): '1234567' -> error, '12' -> '12   '
+varchar(5): '1234567' -> error, '12' -> '12'
+
+text -> max 1GB
+
+*/
+
+CREATE TABLE users(
+    first_name varchar(64),
+    last_name varchar(64),
+    biography text
+);
