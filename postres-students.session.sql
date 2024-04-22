561a7411b6f5f3509127271287579ee9baac856c
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-04-22 19:24:39 +0300

    Composite primary key

--- postres-students.session.sql
+++ postres-students.session.sql
@@ -1,27 +1,27 @@
-DROP TABLE messages;
-
-CREATE TABLE messages(
-    id serial PRIMARY KEY,
-    body text NOT NULL CHECK (body != ''),
-    author varchar(256) NOT NULL CHECK (author != ''),
-    created_at timestamp DEFAULT current_timestamp,
-    is_read boolean DEFAULT false
-);
-
--- INSERT INTO messages VALUES('Hello John.', 'Me');
-
-INSERT INTO messages(author, body) VALUES
-('John', 'Hello.'),
-('Me', 'Go to coffee!'),
-('John', 'Go.');
-
-INSERT INTO messages(author, body) VALUES
-('Peter', 'Hello.'),
-('Peter', 'Hello.');
-
-INSERT INTO messages(id, body, author) VALUES (
-    NULL, 'Message text 4', 'Message author 4'
-); -- тут повернеться помилка
-
 -- Ключ - ознака (найчастіше штучна) яка відрізняє один запис у таблиці від іншого
 -- Первинний ключ (PRIMARY KEY) - використовується для того, щоб ключу дати обмеження унікальності (UNIQUE) і обмеження NOT NULL
+
+
+/* Первинні ключі на два стовпці */
+
+CREATE TABLE coordinates(
+    x int,
+    y int,
+    z int,
+    CONSTRAINT "unique_coord" PRIMARY KEY(x, y, z)
+);
+
+-- (111, 22, 36);
+-- (252, 76, 88);
+-- (111, 22, 36); <<---- ТУТ МАЄ ПОВЕРНУТИСЬ ПОМИЛКА
+
+INSERT INTO coordinates VALUES
+(111, 22, 36),
+(252, 76, 88);
+
+INSERT INTO coordinates VALUES
+(111, 22, 36); -- ТУТ ПОВЕРНУЛАСЬ ПОМИЛКА =)
+
+INSERT INTO coordinates VALUES
+(123, 456, 789),
+(33, 722, 999);
