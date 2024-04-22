a7741e833fc05a209a862e92005d7e675a73ebe0
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-04-22 20:38:16 +0300

    Alter table

--- postres-students.session.sql
+++ postres-students.session.sql
@@ -2,26 +2,45 @@
 -- Первинний ключ (PRIMARY KEY) - використовується для того, щоб ключу дати обмеження унікальності (UNIQUE) і обмеження NOT NULL
 
 
-/* Первинні ключі на два стовпці */
+/* ALTER TABLE */
 
-CREATE TABLE coordinates(
-    x int,
-    y int,
-    z int,
-    CONSTRAINT "unique_coord" PRIMARY KEY(x, y, z)
+CREATE TABLE products(
+    id serial PRIMARY KEY,
+    brand varchar(200) NOT NULL CHECK (brand != ''),
+    model varchar(300) NOT NULL CHECK (model != ''),
+    description text,
+    category varchar(200) NOT NULL CHECK (category != ''),
+    price numeric(10, 2) NOT NULL CHECK (price > 0),
+    discounted_price numeric(10, 2) CHECK (discounted_price <= price)
 );
 
--- (111, 22, 36);
--- (252, 76, 88);
--- (111, 22, 36); <<---- ТУТ МАЄ ПОВЕРНУТИСЬ ПОМИЛКА
+DROP TABLE products;
 
-INSERT INTO coordinates VALUES
-(111, 22, 36),
-(252, 76, 88);
+INSERT INTO products (brand, model, category, price) VALUES
+('Samsung', 'S10', 'smartphones', 200),
+('Iphone', '15 Pro', 'smartphones', 1200),
+('Sony', '456', 'TV', 300),
+('Sony', '457', 'TV', 600);
 
-INSERT INTO coordinates VALUES
-(111, 22, 36); -- ТУТ ПОВЕРНУЛАСЬ ПОМИЛКА =)
 
-INSERT INTO coordinates VALUES
-(123, 456, 789),
-(33, 722, 999);
+ALTER TABLE products
+ADD CONSTRAINT "unique_brand_model_pair" UNIQUE(brand, model);
+
+
+INSERT INTO products (brand, model, category, price) VALUES
+('Iphone', '15 Pro', 'smartphones', 500); -- створити клона не вийде, буде помилка =)
+
+
+ALTER TABLE products
+ADD COLUMN quantity int;
+
+
+ALTER TABLE products
+ADD CONSTRAINT "products_quantity_check" CHECK(quantity >= 0);
+
+
+ALTER TABLE products
+DROP CONSTRAINT "products_quantity_check";
+
+ALTER TABLE products
+DROP COLUMN quantity;
