860b6cd184f1d06a5575e799ff9079c780d4c234
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-04-22 19:08:31 +0300

    Primary key

--- postres-students.session.sql
+++ postres-students.session.sql
@@ -1,22 +1,27 @@
-CREATE TABLE users(
-    first_name varchar(64) NOT NULL CONSTRAINT first_name_not_empty CHECK (first_name != ''),
-    last_name varchar(64) NOT NULL CONSTRAINT last_name_not_empty CHECK (last_name != ''),
-    email varchar(64) NOT NULL CONSTRAINT email_not_empty CHECK (email != '') UNIQUE,
-    gender varchar(30) NOT NULL CONSTRAINT gender_not_empty CHECK (gender != ''),
-    is_subscribe boolean NOT NULL,
-    birthday date CHECK (birthday <= current_date),
-    foot_size smallint,
-    height numeric(5, 2) CONSTRAINT too_high_user CHECK(height < 2.4)
+DROP TABLE messages;
+
+CREATE TABLE messages(
+    id serial PRIMARY KEY,
+    body text NOT NULL CHECK (body != ''),
+    author varchar(256) NOT NULL CHECK (author != ''),
+    created_at timestamp DEFAULT current_timestamp,
+    is_read boolean DEFAULT false
 );
 
-DROP TABLE users;
+-- INSERT INTO messages VALUES('Hello John.', 'Me');
 
-INSERT INTO users VALUES
-('Susan', 'Doe', 'brauni14678@gmail.com', 'female', true, '1994-09-14', 40, 1.65),
-('Peter', 'Doe', 'fwerty1234@mail.ua', 'male', false, '1990-09-14', 46, 1.95);
+INSERT INTO messages(author, body) VALUES
+('John', 'Hello.'),
+('Me', 'Go to coffee!'),
+('John', 'Go.');
 
-INSERT INTO users VALUES
-('Draco', 'Doe', 'brauni@gmail.com', 'male', true, NULL, NULL, 2.3);
+INSERT INTO messages(author, body) VALUES
+('Peter', 'Hello.'),
+('Peter', 'Hello.');
 
-INSERT INTO users VALUES
-('Peter', 'Parker', 'brauni123456789@gmail.com', 'male', true, '2024-04-18', NULL, 2.3);
+INSERT INTO messages(id, body, author) VALUES (
+    NULL, 'Message text 4', 'Message author 4'
+); -- тут повернеться помилка
+
+-- Ключ - ознака (найчастіше штучна) яка відрізняє один запис у таблиці від іншого
+-- Первинний ключ (PRIMARY KEY) - використовується для того, щоб ключу дати обмеження унікальності (UNIQUE) і обмеження NOT NULL
