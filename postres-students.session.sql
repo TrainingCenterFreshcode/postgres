92fd89b335a87f220115dd5871e5e0e47b6babcb
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-04-23 18:35:42 +0300

    Book tasks resolve

--- postres-students.session.sql
+++ postres-students.session.sql
@@ -1,46 +1,52 @@
 -- Ключ - ознака (найчастіше штучна) яка відрізняє один запис у таблиці від іншого
 -- Первинний ключ (PRIMARY KEY) - використовується для того, щоб ключу дати обмеження унікальності (UNIQUE) і обмеження NOT NULL
 
+/*
 
-/* ALTER TABLE */
++ Створити таблицю books
 
-CREATE TABLE products(
+В книги є:
+1. Порядковий номер
+2. Автор книги
+3. Назва книги
+4. Рік випуску
+5. Видавництво
+6. Категорія
+7. Синопсис
+8. Кількість примірників
+9. Статус  -- книга видана / не видана (надрукували її чи ні)
+
+ALTER:
+
++ 1. Додати до таблиці обмеження, що кількість примірників (пункт 8) не може бути менше 0
+
+* + 2. Додати до таблиці обмеження, що автор + назва книги не може бути пустим рядком і має бути унікальним
+
+*/
+
+CREATE TABLE books(
     id serial PRIMARY KEY,
-    brand varchar(200) NOT NULL CHECK (brand != ''),
-    model varchar(300) NOT NULL CHECK (model != ''),
-    description text,
-    category varchar(200) NOT NULL CHECK (category != ''),
-    price numeric(10, 2) NOT NULL CHECK (price > 0),
-    discounted_price numeric(10, 2) CHECK (discounted_price <= price)
+    author varchar(256),
+    name varchar(300),
+    year varchar(4),
+    publisher varchar(256),
+    category varchar(256),
+    synopsys text,
+    quantity int,
+    status boolean
 );
 
-DROP TABLE products;
+ALTER TABLE books
+ADD CONSTRAINT "quantity_more_zero" CHECK (quantity >= 0);
 
-INSERT INTO products (brand, model, category, price) VALUES
-('Samsung', 'S10', 'smartphones', 200),
-('Iphone', '15 Pro', 'smartphones', 1200),
-('Sony', '456', 'TV', 300),
-('Sony', '457', 'TV', 600);
+ALTER TABLE books
+ADD CONSTRAINT "author_name_unique" UNIQUE(author, name);
 
+INSERT INTO books(author, name, quantity) VALUES
+('Оноре Де Бальзак', 'Гобсек', 200);
 
-ALTER TABLE products
-ADD CONSTRAINT "unique_brand_model_pair" UNIQUE(brand, model);
+INSERT INTO books(author, name, quantity) VALUES
+('Оскар Уайльд', 'Портрет Доріана Грея', 0);
 
-
-INSERT INTO products (brand, model, category, price) VALUES
-('Iphone', '15 Pro', 'smartphones', 500); -- створити клона не вийде, буде помилка =)
-
-
-ALTER TABLE products
-ADD COLUMN quantity int;
-
-
-ALTER TABLE products
-ADD CONSTRAINT "products_quantity_check" CHECK(quantity >= 0);
-
-
-ALTER TABLE products
-DROP CONSTRAINT "products_quantity_check";
-
-ALTER TABLE products
-DROP COLUMN quantity;
+INSERT INTO books(author, name, quantity) VALUES
+('Оноре Де Бальзак', 'Шагренева шкіра', 1500);
