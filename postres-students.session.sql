592ec4c0875719ba5e293bd5a1af2e32ead81bc7
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-04-25 20:43:12 +0300

    Cascade delete, update

--- postres-students.session.sql
+++ postres-students.session.sql
@@ -2,36 +2,24 @@
 -- Первинний ключ (PRIMARY KEY) - використовується для того, щоб ключу дати обмеження унікальності (UNIQUE) і обмеження NOT NULL
 -- Потенційний ключ - стовпець (група стовпців) які могли стати первинним ключем, але ще не обрані як такі.
 -- Зовнішній ключ (foreign key) - стовпець (група стовпців) які містять значення, які посилаються на ідентифікатори в інших таблицях
+-- Каскадне видалення - якщо ви видаляєте запис у батьківській таблиці, то автоматично будуть видалені всі записи у дочірніх таблицях, які мають посилання на цей видалений запис.
+-- Каскадне оновлення - якщо значення у записі батьківської таблиці змінюється, то всі відповідні записи у дочірніх таблицях автоматично змінюються згідно цих змін
 
-
--- 1:1
-
-
-CREATE TABLE coaches(
-    id serial PRIMARY KEY,
-    name varchar(256) NOT NULL CHECK(name != '')
+CREATE TABLE orders(
+    ...
+    user_id int REFERENCES user(id) ON UPDATE CASCADE ON DELETE RESTRICT 
+    ...
 );
 
-CREATE TABLE teams(
-    id serial PRIMARY KEY,
-    name varchar(256),
-    coach_id int REFERENCES coaches(id)
+DROP TABLE reactions;
+
+CREATE TABLE reactions(
+    content_id int REFERENCES contents(id) ON DELETE CASCADE,
+    user_id int REFERENCES users(id),
+    is_liked boolean
 );
 
--- + 1. Спочатку створюємо тренерів без зовнішнього ключа
--- + 2. Створюємо таблицю команд (з зовнішнім ключем)
--- + 3. За допомогою ALTER додати зовнішній ключ до таблиці coaches
+INSERT INTO reactions VALUES
+(1, 1, false);
 
-ALTER TABLE coaches
-ADD COLUMN team_id int REFERENCES teams(id);
-
--------
-
-INSERT INTO coaches(name, team_id) VALUES
-('Ivanov', NULL);
-
-INSERT INTO teams(name, coach_id) VALUES
-('Monreal', 1);
-
--- Оновлення інформації в таблиці коучів
--- коуч має відображати, яку команду він тренує (стовпець team_id)
+DELETE FROM contents WHERE id = 1;
