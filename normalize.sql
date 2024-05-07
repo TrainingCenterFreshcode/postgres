b8a8da8273ec6a83453d0b5677879f22929b2859
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-05-07 20:50:11 +0300

    Normalization

---
+++ normalize.sql
@@ -0,0 +1,25 @@
+DROP TABLE employees;
+
+CREATE TABLE employees(
+    id serial PRIMARY KEY,
+    name varchar(200),
+    position varchar(300) REFERENCES positions(name)
+);
+
+INSERT INTO employees (name, position) VALUES
+('John', 'JS developer'),
+('Jane', 'Sales manager'),
+('Jake', 'Bodyguard for developers'),
+('Andrew', 'Driver');
+
+CREATE TABLE positions(
+    name varchar(300) PRIMARY KEY,
+    department varchar(300),
+    car_aviability boolean
+);
+
+INSERT INTO positions (name, car_aviability) VALUES
+('JS developer', false),
+('Sales manager', false),
+('Bodyguard for developers', true),
+('Driver', true);
