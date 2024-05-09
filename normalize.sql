95191c54fb2001f0764830f60d1bbb1696e6fbb0
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-05-09 18:47:17 +0300

    Normalization 1, 2 NF

--- normalize.sql
+++ normalize.sql
@@ -1,6 +1,33 @@
+-- 1 НФ: в кожній комірці зберігається атомарне значення (без списків ітп)
+-- 2 НФ: 1 НФ + таблиця повинна мати ключ; ключ функціонально визначає всю строку
+
+
+CREATE TABLE employees( -- BAD PRACTICE; не відповідає 2 НФ
+    id serial PRIMARY KEY,
+    name varchar(200),
+    department varchar(300),
+    position varchar(300),
+    car_aviability boolean
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 DROP TABLE employees;
 
-CREATE TABLE employees(
+CREATE TABLE employees( -- відповідає 2 НФ
     id serial PRIMARY KEY,
     name varchar(200),
     position varchar(300) REFERENCES positions(name)
@@ -12,7 +39,13 @@ INSERT INTO employees (name, position) VALUES
 ('Jake', 'Bodyguard for developers'),
 ('Andrew', 'Driver');
 
-CREATE TABLE positions(
+INSERT INTO employees (name, position) VALUES
+('Milena', 'CFO'),
+('Sergey', 'CEO'),
+('Matthew', 'SMM/PR division'),
+('Timofey', 'Accountant');
+
+CREATE TABLE positions( -- відповідає 2 НФ
     name varchar(300) PRIMARY KEY,
     department varchar(300),
     car_aviability boolean
@@ -23,3 +56,14 @@ INSERT INTO positions (name, car_aviability) VALUES
 ('Sales manager', false),
 ('Bodyguard for developers', true),
 ('Driver', true);
+
+INSERT INTO positions (name, car_aviability) VALUES
+('CFO', true),
+('CEO', true),
+('SMM/PR division', false),
+('Accountant', false);
+
+
+SELECT employees.id, employees.name, employees.position, positions.car_aviability FROM 
+employees JOIN positions
+ON employees.position = positions.name;
