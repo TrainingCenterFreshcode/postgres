bcd0fd0a3d5c1dab59d817d8e5c7a0b0cf430409
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-05-13 18:33:27 +0300

    Materialized views

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -1,38 +1,39 @@
-
-
 /*
 
-
-CREATE FUNCTION function_name ([parameter1 data_type [, parameter2 data_type, ...]])
-RETURNS return_data_type
-AS
-$$
-BEGIN
-
-    -- Тіло функції (операції)
-    RETURN result;
-
-END;
-$$
-LANGUAGE plpgsql; -- Мова програмування функції для Postgresql
+Задача: порахувати кількість замовлень
 
 */
 
-CREATE FUNCTION add_numbers(number1 integer, number2 integer)
-RETURNS integer
+CREATE MATERIALIZED VIEW total_orders AS (
+    SELECT count(*) AS "загальна кількість замовлень" 
+    FROM orders
+);
+
+DROP MATERIALIZED VIEW total_orders;
+
+SELECT * FROM total_orders; -- 6956
+
+INSERT INTO orders (customer_id, status)
+VALUES (
+    6002,
+    false
+);
+
+REFRESH MATERIALIZED VIEW total_orders;
+
+
+
+CREATE FUNCTION refresh_materialized_view()
+RETURNS void
 AS
 $$
 BEGIN
-    RETURN number1 + number2;
+    REFRESH MATERIALIZED VIEW total_orders;
 END;
 $$
 LANGUAGE plpgsql;
 
--- Виклик функції
+DROP FUNCTION refresh_materialized_view();
 
-SELECT add_numbers(5, 15);
 
--- Видалення функції
-
-DROP FUNCTION add_numbers;
-DROP FUNCTION add_numbers(integer, integer);
+SELECT refresh_materialized_view();
