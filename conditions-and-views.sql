d007fcd2291d25cd52b2c2eedb95337ad4bd95f8
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-05-13 19:20:42 +0300

    Create schema

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -1,45 +1,7 @@
-/*
+CREATE SCHEMA new_schema;
 
-order -> status:
-- true = виконано
-- false = не виконано
-- ('new', 'processing', 'shiped', 'done', 'canceled')
-
-*/
-
-INSERT INTO orders(customer_id, status) VALUES
-(6003, 'new');
-
-CREATE TYPE order_status AS ENUM('new', 'processing', 'shiped', 'done', 'canceled');
-
-
-ALTER TABLE orders
-ALTER COLUMN status 
-TYPE order_status
-USING (
-    CASE status
-        WHEN false THEN 'processing'
-        WHEN true THEN 'done'
-        ELSE 'new'
-    END
-)::order_status;
-
-
-
-SELECT * FROM orders
-ORDER BY created_at DESC;
-
-
-UPDATE orders
-SET status = 'done'
-WHERE id = 12956;
-
-
-
-
-
-
-
-
-DROP VIEW users_with_orders_amount;
-DROP VIEW orders_with_price;
+CREATE TABLE new_schema.users(
+    first_name varchar(20) NOT NULL CHECK(first_name != ''),
+    last_name varchar(20) NOT NULL CHECK(last_name != ''),
+    email varchar(20) NOT NULL CHECK(email != '')
+);
